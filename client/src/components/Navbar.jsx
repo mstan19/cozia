3445e4e0e0a8f3f13fadf9f293d3aa43571ac2d9
Christy Le <christylex3@gmail.com>
2023-01-15 00:35:27 -0500

    Fixed navbar to be working for desktop more properly

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,21 +12,22 @@ export default function Navbar() {
 
     return (
         <header>
-            <h1>Cozia</h1>
+            <h1 className="app-title">Cozia</h1>
             <nav ref={navRef}>
-                <Link className="nav-link" to="/myaccount">My Account</Link>
-                <Link className="nav-link" >Categories</Link>
-                <Link className="nav-link" >Sales & Clearance</Link>
-                <Link className="nav-link" >Trending</Link>
+                <Link className="nav-link" to="/myaccount">
+                    My Account
+                </Link>
+                <Link className="nav-link">Categories</Link>
+                <Link className="nav-link">Sales & Clearance</Link>
+                <Link className="nav-link">Trending</Link>
                 {/* Used to closed navbar */}
                 <button className="nav-btn nav-close-btn" onClick={showNavbar}>
                     <FaTimes />
                 </button>
             </nav>
             <button className="nav-btn" onClick={showNavbar}>
-                    <FaBars />
+                <FaBars />
             </button>
         </header>
-        // 13:36
     );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,9 +4,9 @@ import Navbar from "../components/Navbar";
 const Home = () => {
 
     return (
-        <div>
+        <main>
             <Navbar />
-        </div>
+        </main>
     );
 }
 
