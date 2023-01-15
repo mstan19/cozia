775e8efcf90f2d3e92d6d3409465090a8eb838d4
Christy Le <christylex3@gmail.com>
2023-01-15 01:16:20 -0500

    Fixed hamburger icon to be on the left, app's title in the center, and the wishlist and shopping icon on the right

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { AiFillHeart, AiFillShopping } from "react-icons/ai";
 import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -12,22 +13,32 @@ export default function Navbar() {
 
     return (
         <header>
-            <h1 className="app-title">Cozia</h1>
             <nav ref={navRef}>
                 <Link className="nav-link" to="/myaccount">
                     My Account
                 </Link>
                 <Link className="nav-link">Categories</Link>
+                {/* <div className="flex flex-row justify-space-between">
+                    <h2>Women</h2>
+                    <h2>Men</h2>
+                </div> */}
                 <Link className="nav-link">Sales & Clearance</Link>
                 <Link className="nav-link">Trending</Link>
-                {/* Used to closed navbar */}
+                {/* Closed expanded navbar */}
                 <button className="nav-btn nav-close-btn" onClick={showNavbar}>
                     <FaTimes />
                 </button>
             </nav>
+            {/* Hamburger icon */}
             <button className="nav-btn" onClick={showNavbar}>
                 <FaBars />
             </button>
+            <h1 className="app-title">Cozia</h1>
+            {/* Wishlist and Shopping icons */}
+            <section className="nav-btn nav-shop-btn flex flex-row place-content-evenly">
+                <Link><AiFillHeart /></Link>
+                <Link><AiFillShopping /></Link>
+            </section>
         </header>
     );
 }
