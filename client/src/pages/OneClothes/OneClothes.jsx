782e191324d6525bafc7a97f0714b2da250b196d
Christy Le <christylex3@gmail.com>
2023-03-01 11:42:32 -0500

    Connecting individual clothes to its page. Reformatting other files (MyProducts, SalesItem, OrderList)

---
+++ client/src/pages/OneClothes/OneClothes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+
+const OneClothes = () => {
+
+    const { clothesId } = useParams();
+
+    return (
+        <main>
+            {/* <img src={image} alt={productName} /> */}
+        </main>
+    );
+
+}
+
+export default OneClothes;
