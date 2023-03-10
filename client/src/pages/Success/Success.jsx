46164303d36e6b52cda862e9123c24b95a4f2594
melissa <melissastan91@gmail.com>
2023-03-09 23:01:35 -0500

    ispaid is set to true after success

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -1,17 +1,58 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { useMutation } from "@apollo/client";
+import { EDIT_ORDER } from "../../utils/mutations";
 import { useParams  } from "react-router-dom";
+import { CartState } from "../../context/CartContext";
 
-import { QUERY_ALLORDERS } from "../../utils/queries";
+
 
 const Success = () => {
 //   const [setPaidTrue] = useMutation(SET_PAID_TRUE);
   const { orderID } = useParams();
-  console.log(orderID)
-  localStorage.setItem("product", JSON.stringify([]));
-  localStorage.removeItem("orderData");
+  const [editOrder] = useMutation(EDIT_ORDER);
+  const [updateOrder, setUpdateOrder] = useState({
+		deliveryDate: "",
+		deliveryStatus: "",
+	});
 
+  let updatepayment = {
+    isPaid: true
+  }
+  // const handleEditOrderBtn =  () => {
+  const updatedOrder =  editOrder({
+    variables: {
+      orderId: orderID,
+      orderData: updatepayment,
+    },
+  });
+// }
+// handleEditOrderBtn();
+  // const { cart, setCart } = CartState();
+  console.log(orderID)
+  
+//   JSON.parse(localStorage.getItem("product"))
+// useEffect (() => {
+//          JSON.parse(localStorage.getItem("product"))
+//        console.log(JSON.parse(localStorage.getItem("product")))
+//     //    cart = []
+//         localStorage.setItem(("product"), JSON.stringify(cart))
+// console.log(JSON.parse(localStorage.getItem("product")))
+
+// }, [cart])
+
+
+let keysToRemove = ["orderData","product"];
+
+    keysToRemove.forEach(k =>
+        localStorage.removeItem(k)
+       );
+       
+    
+    //  localStorage.removeItem("product");
+    //  localStorage.removeItem("orderData");
+  
+//   localStorage.removeItem("product");
   const handleRedirectDashboard = () => {
     window.location.assign("/dashboard");
 };
@@ -20,7 +61,7 @@ const Success = () => {
     <div className="h-screen flex flex-col items-center">
       <h1>Success!</h1>
       <h2>Thank you for your purchase!</h2>
-      <h2>Click the Dashboard so you can redirected to your dashboard!</h2>
+      <h2>Redirecting you to your dashboard!</h2>
       <div className="items-center justify-between">
 						<button
 							className="bg-green-600 w-full rounded-sm hover:bg-green-600 text-white mt-4 py-2 px-4 focus:outline-none"
