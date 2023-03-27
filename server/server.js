5a157df932e5d37565c62e2bcf1b6233eed293ad
melissa <melissastan91@gmail.com>
2023-03-27 16:43:37 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -27,9 +27,9 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-}
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
 app.get("/*", function (req, res) {
     res.sendFile(
